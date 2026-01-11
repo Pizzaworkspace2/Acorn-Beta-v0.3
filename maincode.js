4f448ee0cd414f26553c3b0e454713c75117ba66
Marco <5439724524@student.cms.k12.nc.us>
2026-01-11 16:58:26 -0500

    Show characters on play button click

--- maincode.js
+++ maincode.js
@@ -4,5 +4,15 @@ document.addEventListener('DOMContentLoaded', (event) => {
 const acorntitle = document.getElementById('home');
 const characters = document.getElementById('characters');
 const play = document.getElementById('play');
+
+  characters.style.display = 'none';
+
+  play.addEventListener('click', function() {
+    acorntitle.style.display = 'none';
+    characters.style.display = 'block';
+
+  };
+};
+    
   
                                            
